f27541cc1593843e84d5274459a55984c4f5a557
Rickard Borg <gramas19@live.se>
2025-07-13 17:38:39 +0300

    Added optional save function to save extracted words

--- FileHandler.py
+++ FileHandler.py
@@ -2,14 +2,29 @@ from colorama import Fore
 import os
 
 class FileHandler:
-    def __init__(self):
+    def __init__(self, cli):
+        self.cli = cli
         self.api_endpoint_file = "api-endpoints.txt"
         self.endpoints = self._load_existing_endpoints(self.api_endpoint_file)
+        if self.cli.args.output:
+            self.words = self._load_existing_endpoints(self.cli.args.output)
+        else:
+            self.words = set()
 
     # Filetype: text, html, json, xml
-    def save(self, filename, filetype="text"):
-        # TODO: Save result to a custom file
-        pass
+    # TODO: add extension check
+    def save(self, words, filetype="text"):
+        if self.cli.args.output:
+            new_words = set(words) - self.words  # endast nya ord
+            if not new_words:
+                return
+            try:
+                with open(self.cli.args.output, "a") as f:
+                    for word in new_words:
+                        f.write(word + "\n")
+                        self.words.add(word)  # uppdatera interna listan också
+            except Exception as e:
+                print(Fore.RED + f"Error writing to file: {e}" + Fore.WHITE)
 
     def save_api_endpoints(self, candidates):
         api_common_names = [
@@ -29,14 +44,14 @@ class FileHandler:
         for endpoint in candidates:
             if any(name in endpoint for name in api_common_names) and endpoint not in self.endpoints:
                 self._write_to_file(self.api_endpoint_file, endpoint)
-                print(endpoint)
         
     def _write_to_file(self, filename, endpoint):
         try:
             with open(filename, "a") as f:
                 f.write(endpoint + "\n")
             self.endpoints.add(endpoint)
-            print(Fore.GREEN + f"Saved: {endpoint}" + Fore.WHITE)
+            if self.cli.args.verbose:
+                print(Fore.GREEN + f"Saved: {endpoint}" + Fore.WHITE)
         except FileNotFoundError:
             print(Fore.YELLOW + "File not found. Creating api-endpoints.text" + Fore.WHITE)
         except PermissionError:
