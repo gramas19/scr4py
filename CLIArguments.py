5182e11454c52675e88775367ddb78a5aed08f69
Rickard Borg <gramas19@live.se>
2025-07-13 17:40:09 +0300

    Made some changes to arguments that are required, arguments that don't need values etc

--- CLIArguments.py
+++ CLIArguments.py
@@ -18,12 +18,12 @@ class CLIArguments:
         # TODO: Add more arguments to the list
         # Verbose mode, output-file, output format, scan all option, ratelimit, headers, random user agent,
         # disable SSL
-        self.parser.add_argument("-u", "--url", help="The URL to scan")
-        self.parser.add_argument("-v", "--verbose", help="Enable detailed output")
-        self.parser.add_argument("-o", "--output-file", help="Write result to a file")
-        self.parser.add_argument("-of", "--output-format", help="output format: text, json, html")
-        self.parser.add_argument("-A", "-all", help="Scan everything")
+        self.parser.add_argument("-u", "--url", help="The URL to scan", required=True)
+        self.parser.add_argument("-v", "--verbose", help="Enable detailed output", action="store_true")
+        self.parser.add_argument("-o", "--output-file", help="Write result to a file", dest="output")
+        self.parser.add_argument("-of", "--output-format", help="output format: text, json, html", choices=["text", "json", "html"], default="text")
+        self.parser.add_argument("-A", "-all", help="Scan everything", action="store_true")
         self.parser.add_argument("--rate", help="Set rate limit")
         self.parser.add_argument("-H", "--header", help="Set request headers")
-        self.parser.add_argument("--random-user-agent", help="Randomize the user-agent each request")
-        self.parser.add_argument("--ignore", help="Disable SSL verification")
+        self.parser.add_argument("--random-user-agent", help="Randomize the user-agent each request", action="store_true", dest="rua")
+        self.parser.add_argument("--ignore", help="Disable SSL verification", action="store_true")
